fc4e272aa8f75d7f4ef3e416570f707a08ae5e38
Joseph Hall <121320782+JosephMattHall@users.noreply.github.com>
2023-08-26 22:15:48 -0700

    Replace accidentally deleted part of the file.

--- app.py
+++ app.py
@@ -147,3 +147,148 @@ async def create_companion_route(request):
 
     await create_companion(data, user_id)
     return response.json({"message": "Companion created successfully"}, status=201)
+
+# Helper function to start a training course for a companion
+@app.put("/companion/start_training/<companion_id>")
+async def start_training_course(request, companion_id):
+    # Get the user_id from the Authorization token
+    auth_header = request.headers.get("Authorization")
+    user_id = get_user_id_from_token(auth_header)
+    if not user_id:
+        return response.json({"error": "Unauthorized"}, status=401)
+
+    # Check if the companion belongs to the user
+    companion = await db.companions.find_one({"_id": ObjectId(companion_id), "user_id": user_id})
+    if not companion:
+        return response.json({"error": "Companion not found or does not belong to the user"}, status=404)
+
+    # Check if the companion is already training
+    if "training_end_time" in companion:
+        return response.json({"error": "Companion is already undergoing training"}, status=400)
+
+    # Check if the chosen_stat is valid (should be one of "strength", "defense", or "speed")
+    chosen_stat = request.json.get("chosen_stat")
+    if chosen_stat not in ["strength", "defense", "speed"]:
+        return response.json({"error": "Invalid chosen_stat"}, status=400)
+
+    # Calculate the training duration based on the companion's level
+    training_duration = calculate_training_duration(companion["level"])
+
+    # Calculate the training end time
+    training_end_time = datetime.utcnow() + datetime.timedelta(seconds=training_duration)
+
+    # Update the companion document with the training_end_time and chosen_stat
+    await db.companions.update_one(
+        {"_id": ObjectId(companion_id)},
+        {"$set": {"training_end_time": training_end_time, "chosen_stat": chosen_stat}}
+    )
+
+    return response.json({"message": "Training course started successfully", "end_time": training_end_time})
+
+
+# Helper function to finish a training course for a companion
+@app.put("/companion/finish_training/<companion_id>")
+async def finish_training_course(request, companion_id):
+    # Get the user_id from the Authorization token
+    auth_header = request.headers.get("Authorization")
+    user_id = get_user_id_from_token(auth_header)
+    if not user_id:
+        return response.json({"error": "Unauthorized"}, status=401)
+
+    # Check if the companion belongs to the user
+    companion = await db.companions.find_one({"_id": ObjectId(companion_id), "user_id": user_id})
+    if not companion:
+        return response.json({"error": "Companion not found or does not belong to the user"}, status=404)
+
+    # Check if the companion is actually undergoing training
+    if "training_end_time" not in companion:
+        return response.json({"error": "Companion is not undergoing any training"}, status=400)
+
+    # Check if the training course is finished
+    if companion["training_end_time"] > datetime.utcnow():
+        return response.json({"error": "Training course is not yet completed"}, status=400)
+
+    # Apply training bonuses
+    if "chosen_stat" in companion:
+        chosen_stat = companion["chosen_stat"]
+        await db.companions.update_one({"_id": ObjectId(companion_id)}, {"$inc": {chosen_stat: 1}})
+
+        # Calculate the bonus chance (10% chance of getting a bonus)
+        if random.random() <= 0.1:
+            await db.companions.update_one({"_id": ObjectId(companion_id)}, {"$inc": {chosen_stat: 2}})
+
+        # Remove the chosen_stat from the companion document
+        await db.companions.update_one({"_id": ObjectId(companion_id)}, {"$unset": {"chosen_stat": 1}})
+
+    # Reset the training_end_time and update the companion's document
+    await db.companions.update_one({"_id": ObjectId(companion_id)}, {"$unset": {"training_end_time": 1}})
+
+    return response.json({"message": "Training course finished successfully"})
+
+
+# Helper function to get the training status of all companions for a user
+@app.route("/companion/training_status", methods=["GET"])
+async def training_status(request):
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Bearer "):
+        return response.json({"error": "Unauthorized"}, status=401)
+
+    token = auth_header.split(" ")[1]
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+    except jwt.ExpiredSignatureError:
+        return response.json({"error": "Token has expired"}, status=401)
+    except jwt.InvalidTokenError:
+        return response.json({"error": "Invalid token"}, status=401)
+
+    user_id = payload.get("user_id")
+    if not user_id:
+        return response.json({"error": "User ID not found in token"}, status=401)
+
+    companions = await db.companions.find({"user_id": user_id}).to_list(None)
+
+    training_status = {}
+    for companion in companions:
+        name = companion["name"]
+        if "training_end_time" in companion:
+            end_time = companion["training_end_time"]
+            time_remaining = max((end_time - datetime.utcnow()).total_seconds(), 0)
+            status = "training" if time_remaining > 0 else "finish"
+            training_status[name] = {
+                "status": status,
+                "time_remaining": time_remaining,
+                "chosen_stat": companion.get("chosen_stat")  # Include the chosen_stat in the response
+            }
+        else:
+            training_status[name] = {"status": "ready"}
+
+    return response.json({"training_status": training_status})
+
+
+@app.route("/protected-route", methods=["GET"])
+async def protected_route(request):
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Bearer "):
+        return response.json({"error": "Unauthorized"}, status=401)
+
+    token = auth_header.split(" ")[1]
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+    except jwt.ExpiredSignatureError:
+        return response.json({"error": "Token has expired"}, status=401)
+    except jwt.InvalidTokenError:
+        return response.json({"error": "Invalid token"}, status=401)
+
+    return response.json({"message": "Protected route accessed successfully"})
+
+
+@app.listener("before_server_start")
+async def setup_app(app, loop):
+    global db
+    client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
+    db = client[MONGODB_DBNAME]
+    await db.companions.create_index("name", unique=True)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
